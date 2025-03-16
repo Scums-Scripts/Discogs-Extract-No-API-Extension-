c4de77a11d562f04cfe228a8341e0d82c7707822
Scums-Scripts <scumsclothingco@gmail.com>
2025-03-16 12:16:42 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -96,8 +96,8 @@ class SquareItemExtractor {
       'Default Unit Cost': '',
       'Default Vendor Name': '',
       'Default Vendor Code': '',
-      'Current Quantity': '',
-      'New Quantity': settings.stock || 0,
+      'Current Quantity': settings.stock || 0, // Place stock in Current Quantity
+      'New Quantity': '',
       'Stock Alert Enabled': '',
       'Stock Alert Count': '',
       'Tax - Sales Tax (5%)': 'Y'
